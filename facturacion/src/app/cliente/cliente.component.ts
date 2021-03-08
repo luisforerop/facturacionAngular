e5a837c9002dedba41fb00c61ad773dfde3b8014
Luis <Luis@LAPTOP-0EFHT9GS>
2021-03-08 00:11:26 -0500

    Corrección: en /cliente podemos ejecutar consultas de la colección cliente, y en facturación consultas de la colección factura. En el anterior había quedado invertido.

--- facturacion/src/app/cliente/cliente.component.ts
+++ facturacion/src/app/cliente/cliente.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ApiclienteService } from '../services/apicliente.service';
 import { Respuesta } from '../Models/response';
 import { dialogClienteComponent } from './dialog/dialogcliente.component';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { AplidatosService } from '../services/aplidatos.service';
 import { cliente } from '../Models/cliente';
 
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 
 //CADA VEZ QUE CARQUE LA DIRECCIÓN DE CLIENTE, SE VA A EJECUTAR EL PROCESO CODIFICADO ACÁ
 
-console.log("Hemos entrado");
 //var nEmpresa = prompt("Sobre que empresa requiere información");
 
 @Component({
@@ -30,10 +31,12 @@ export class ClienteComponent implements OnInit {
   public lst: any;
 
   //Arreglo de string para especificar las columnas que se van a mostrar
-  public columnas: string[] = [ "id", "empresa", "ciudad", "nit", "total", "subtotal", "iva", "retencion", "estado", "pago", "fechaCreacion", "fechaPago", "editar" ];
+  //public columnas: string[] = [ "id", "empresa", "ciudad", "nit", "total", "subtotal", "iva", "retencion", "estado", "pago", "fechaCreacion", "fechaPago", "editar" ];
+  public columnas: string[] = ["empresa", "ciudad", "nit", "email", "encargado", "editar"];
 
   constructor(
     private apiCliente: ApiclienteService, //El appiclienteservice es la clase que creamos en apicliente.service.ts Así la inyectamos. Con esto básicamente estamos escribiendo todo lo que dice en apicliente. Eso nos ayuda a reciclar nuestras clases.
+    private apiDatos: AplidatosService,
     //Creamos un dialog
     public dialog: MatDialog,
     public snackbar: MatSnackBar,
@@ -54,10 +57,18 @@ export class ClienteComponent implements OnInit {
 
   //Creamos un método para poder refrescar nuestros listados y que no dependa del constructor
   getClientes(gcliente: string) {
-    this.apiCliente.getCliente(gcliente).subscribe(Respuesta => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
-      this.lst = Respuesta;
-      console.log(Respuesta);
+
+    //ESTA ES LA API PARA LOS CLIENTES
+    this.apiDatos.getCliente(gcliente).subscribe(cliente => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
+      this.lst = cliente;
+      console.log(cliente);
     })
+
+   // this.apiCliente.getCliente(gcliente).subscribe(Respuesta => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
+   //   this.lst = Respuesta;
+   //   console.log(Respuesta);
+   // })
+
   }
 
 
@@ -65,9 +76,6 @@ export class ClienteComponent implements OnInit {
   filtro() //Función para darle el uso de filtro al botón de cliente component
   {
     let snackBarFiltro: any
-    //Abrimos nuestro componente dialogcliente
-    console.log("oprimio boton")
-    //console.log(this.pruebita.pasarNombre)
 
     //INICIALMENTE el filtro se creó para que reaccionara con un cuadro de diálogo, pero para mejorar la UX vamos a usar un snackbar
     /*
@@ -79,54 +87,30 @@ export class ClienteComponent implements OnInit {
     */
 
     //EL FILTRO SE EJECUTA TAN PRONTO SE ACTIVA LA SNACKBAR
-    snackBarFiltro = this.snackbar.open('Filtrando por ' + this.nombre, '', { duration: 1500 });
+    snackBarFiltro = this.snackbar.open('Filtrando por ubicación: ' + this.nombre, '', { duration: 1500 });
     snackBarFiltro.afterDismissed().subscribe(() => {
       this.getClientes(this.nombre);
       
     });
   }
 
-  editar(datos: Respuesta) {
-    let snackBarRef: any;
-    
-    //datos.estado = "Recién procesado"; //Comando para reinicializar todo el ciclo
-    
-    switch (datos.estado) {
-      case "Recién procesado":
-        {
-          //datos.estado = "Primer recordatorio";
-          snackBarRef = this.snackbar.open('Primer recordatorio enviado', '', { duration: 1750 });
-          break;
-        }
-      case "Primer recordatorio":
-        {
-          //datos.estado = "Segundo recordatorio";
-          snackBarRef = this.snackbar.open('Segundo recordatorio enviado', '', { duration: 1750 });
-          break;
-        }
-      case "Segundo recordatorio":
-        {
-          //datos.estado = "Desactivado";
-          snackBarRef = this.snackbar.open('La factura se ha desactivado', '', { duration: 1750 })
-          break;
-        }
-      default: {
-        this.snackbar.open('La factura está desactivada', '', { duration: 1500 })
-
-      }
-    }/**/
+  editar(datos: cliente) {
 
     //Ejecuta el método edit
-    this.apiCliente.edit(datos).subscribe(i => console.log(`${i} este es i`));
+    this.apiDatos.edit(datos).subscribe(i => console.log(`${i} este es i`));
     console.log("acabamos de enviar la petición");
-           
+
+    let snackBarRef: any;
+    snackBarRef = this.snackbar.open('Hemos realizado los cambios satisfactoriamente. Actualizando...', '', { duration: 1750 });
+    //datos.estado = "Recién procesado"; //Comando para reinicializar todo el ciclo
+
     //Cuando se cierra el snackbar se actualiza la lista
     snackBarRef.afterDismissed().subscribe(() => {
       this.getClientes(this.nombre);
       console.log('The snack-bar was dismissed');
     });
     //Esta sección tiene opción de mejora para verificar si se actualizo el dato o no, ya que en ocasiones la base de datos puede tardar unos milisegundos en actualizar la información. Se puede implementar un ciclo while que repita la próxima instrucción hasta que sea diferente a la que ingresó
-        
+
   }
 
 
--- facturacion/src/app/facturacion/facturacion.component.ts
+++ facturacion/src/app/facturacion/facturacion.component.ts
@@ -12,7 +12,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 //CADA VEZ QUE CARQUE LA DIRECCIÓN DE CLIENTE, SE VA A EJECUTAR EL PROCESO CODIFICADO ACÁ
 
-console.log("Hemos entrado");
 //var nEmpresa = prompt("Sobre que empresa requiere información");
 
 @Component({
@@ -30,7 +29,9 @@ export class FacturacionComponent implements OnInit {
   public lst: any;
 
   //Arreglo de string para especificar las columnas que se van a mostrar
-  public columnas: string[] = ["empresa", "ciudad", "nit", "email", "encargado", "editar"];
+  public columnas: string[] = ["id", "empresa", "ciudad", "nit", "total", "subtotal", "iva", "retencion", "estado", "pago", "fechaCreacion", "fechaPago", "editar"];
+
+//  public columnas: string[] = ["empresa", "ciudad", "nit", "email", "encargado", "editar"];
 
   constructor(
     private apiCliente: ApiclienteService, //El appiclienteservice es la clase que creamos en apicliente.service.ts Así la inyectamos. Con esto básicamente estamos escribiendo todo lo que dice en apicliente. Eso nos ayuda a reciclar nuestras clases.
@@ -55,11 +56,21 @@ export class FacturacionComponent implements OnInit {
 
   //Creamos un método para poder refrescar nuestros listados y que no dependa del constructor
   getClientes(gcliente: string) {
-    //ESTA ES LA API PARA LOS CLIENTES
-    this.apiDatos.getCliente(gcliente).subscribe(cliente => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
-      this.lst = cliente;
-      console.log(cliente);
+
+    this.apiCliente.getCliente(gcliente).subscribe(Respuesta => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
+      this.lst = Respuesta;
+      console.log(Respuesta);
     })
+
+    //ESTA ES LA API PARA LOS CLIENTES
+    //this.apiDatos.getCliente(gcliente).subscribe(cliente => { /*Con el this especificamos que es el objeto que creamos en el constructor cuando inyectamos apiclient */
+    //  this.lst = cliente;
+    //  console.log(cliente);
+   // })
+
+
+
+
   }
 
 
@@ -88,14 +99,40 @@ export class FacturacionComponent implements OnInit {
     });
   }
 
-  editar(datos: cliente) {
-    //Ejecuta el método edit
-    this.apiDatos.edit(datos).subscribe(i => console.log(`${i} este es i`));
-    console.log("acabamos de enviar la petición");
+  editar(datos: Respuesta) {
 
     let snackBarRef: any;
-    snackBarRef = this.snackbar.open('Hemos realizado los cambios satisfactoriamente. Actualizando...', '', { duration: 1750 });
+
     //datos.estado = "Recién procesado"; //Comando para reinicializar todo el ciclo
+
+    switch (datos.estado) {
+      case "Recién procesado":
+        {
+          //datos.estado = "Primer recordatorio";
+          snackBarRef = this.snackbar.open('Primer recordatorio enviado', '', { duration: 1750 });
+          break;
+        }
+      case "Primer recordatorio":
+        {
+          //datos.estado = "Segundo recordatorio";
+          snackBarRef = this.snackbar.open('Segundo recordatorio enviado', '', { duration: 1750 });
+          break;
+        }
+      case "Segundo recordatorio":
+        {
+          //datos.estado = "Desactivado";
+          snackBarRef = this.snackbar.open('La factura se ha desactivado', '', { duration: 1750 })
+          break;
+        }
+      default: {
+        this.snackbar.open('La factura está desactivada', '', { duration: 1500 })
+
+      }
+    }/**/
+
+    //Ejecuta el método edit
+    this.apiCliente.edit(datos).subscribe(i => console.log(`${i} este es i`));
+    console.log("acabamos de enviar la petición");
 
     //Cuando se cierra el snackbar se actualiza la lista
     snackBarRef.afterDismissed().subscribe(() => {
@@ -103,6 +140,8 @@ export class FacturacionComponent implements OnInit {
       console.log('The snack-bar was dismissed');
     });
     //Esta sección tiene opción de mejora para verificar si se actualizo el dato o no, ya que en ocasiones la base de datos puede tardar unos milisegundos en actualizar la información. Se puede implementar un ciclo while que repita la próxima instrucción hasta que sea diferente a la que ingresó
+
+
 
   }
 
